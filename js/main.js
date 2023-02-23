967adfd673e8b83213a6fdad9e28df90cd58f882
Herenyon <rikitur17@gmail.com>
2023-02-23 14:57:17 +0100

    prima parte form finita con calcoli nella console

--- js/main.js
+++ js/main.js
@@ -1,39 +1,49 @@
-const numeroChilometri = parseInt(document.getElementById('distanza').value);
-const etaPasseggero = parseInt(document.getElementById('fasciaEta')) ;
-
-
-let prezzoBigliettoRidotto;
-let prezzoBiglietto = numeroChilometri * 0.21;
-// console.log(numeroChilometri);
-
-const generaPrezzo = document.getElementById('genera');
+'use strict'
 
 
 
 
 
+const buttonGenerate = document.getElementById('genera');
 
-    
+buttonGenerate.addEventListener('click',
+    function () {
+        const nomeUtente = document.getElementById('nomeCognome');
+        const numeroChilometri = document.getElementById('distanza').value;
+        const etaPasseggero = document.getElementById('fasciaEta');
+
+        const nomeUtenteValue = nomeUtente.value;
+        const numeroChilometriValue = Number(numeroChilometri.value);
+        const etaPasseggeroValue = etaPasseggero.value;
+        let prezzoBigliettoRidotto;
+        let prezzoBiglietto = numeroChilometri * 0.21;
+
+
+        if (etaPasseggeroValue === 'minorenne') {
+            prezzoBigliettoRidotto = prezzoBiglietto * 0.8;
+            prezzoBigliettoRidotto = prezzoBigliettoRidotto.toFixed(2);
+            console.log('il prezzo del tuo biglietto è:' + ' ' + prezzoBigliettoRidotto + '€' + ' ' + etaPasseggeroValue);
+            // document.getElementById("prezzoFinale").innerHTML = prezzoBigliettoRidotto + ' ' + '€';
+        }
+        else if (etaPasseggeroValue === 'over') {
+            prezzoBigliettoRidotto = prezzoBiglietto * 0.6;
+            prezzoBigliettoRidotto = prezzoBigliettoRidotto.toFixed(2);
+            console.log('il prezzo del tuo biglietto è:' + ' ' + prezzoBigliettoRidotto + '€' + ' ' + etaPasseggeroValue);
+            // document.getElementById("prezzoFinale").innerHTML = prezzoBigliettoRidotto + ' ' + '€';
+        }
+
+        else {
+            prezzoBiglietto = prezzoBiglietto.toFixed(2)
+            console.log('il prezzo del tuo biglietto è:' + ' ' + prezzoBiglietto + '€' + ' ' + etaPasseggeroValue);
+            // document.getElementById("prezzoFinale").innerHTML = prezzoBiglietto + ' ' + '€';
+        }
+
+    }
+
+
+)
 
 
 
 
 
-if (etaPasseggero === 'minorenne') {
-    prezzoBigliettoRidotto = prezzoBiglietto * 0.8;
-    prezzoBigliettoRidotto = prezzoBigliettoRidotto.toFixed(2);
-    console.log('il prezzo del tuo biglietto è:' + ' ' + prezzoBigliettoRidotto + '€');
-    // document.getElementById("prezzoFinale").innerHTML = prezzoBigliettoRidotto + ' ' + '€';
-}
-else if (etaPasseggero === 'over') {
-    prezzoBigliettoRidotto = prezzoBiglietto * 0.6;
-    prezzoBigliettoRidotto = prezzoBigliettoRidotto.toFixed(2);
-    console.log('il prezzo del tuo biglietto è:' + ' ' + prezzoBigliettoRidotto + '€');
-    // document.getElementById("prezzoFinale").innerHTML = prezzoBigliettoRidotto + ' ' + '€';
-}
-
-else {
-    prezzoBiglietto = prezzoBiglietto.toFixed(2)
-    console.log('il prezzo del tuo biglietto è:' + ' ' + prezzoBiglietto + '€');
-    // document.getElementById("prezzoFinale").innerHTML = prezzoBiglietto + ' ' + '€';
-}
